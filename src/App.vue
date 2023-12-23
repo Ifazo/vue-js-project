ca5d496c11029eb1ad07144d4fcae7a3a7ea742e
Ifazo <ziaulkarimifaz@outlook.com>
2023-12-23 15:52:57 +0600

    setup init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HeaderComponent from "./components/HeaderComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+</script>
+
+<template>
+  <HeaderComponent />
+  <h1 class="text-3xl font-bold text-center">Hello world!</h1>
+  <RouterView />
+  <FooterComponent />
+</template>
+
+<style scoped></style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    <p>Footer section</p>
+  </footer>
+</template>
